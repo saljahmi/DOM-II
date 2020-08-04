6fea6e3a816f9a802cc0191ca7d946fd733ea67d
saljahmi <saljahmi@gmail.com>
2020-08-04 18:31:51 -0400

    used mouseenet, mouseout, and click events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,53 @@
+// ### Task 2a: Create Unique Event Listeners
+
+// * [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). Use your creativity to make the Fun Bus site more interactive. For example you could change colors, animate objects, remove objects, etc. Here are some unique event types you could try to use:
+//   * `mouseover`
+//   * `keydown`
+//   * `wheel`
+//   * `load`
+//   * `focus`
+//   * `resize`
+//   * `scroll`
+//   * `select`
+//   * `dblclick`
+//   * `drag / drop`
+
+// Note: Drag and drop is a bit more advanced than the others as it's not actually a single type of event but several, working together.
+
+// * [ ] Nest two similar events somewhere in the site and prevent the event propagation properly. Remember not all event types bubble.
+// * [ ] Stop the navigation items from refreshing the page by using `preventDefault()`
+
 // Your code goes here
+
+// mouseenter, mouseout
+
+const topLinks = document.querySelectorAll(".nav-link")
+
+function makePurple(event){
+    event.target.style.color = "purple";
+    console.log('made it putple')
+}
+function makeBlack(event){
+    event.target.style.color = "black"
+}
+
+topLinks.forEach(element => {
+    element.addEventListener('mouseenter', makePurple)
+    element.addEventListener('mouseout', makeBlack)
+})
+
+//click
+
+const mainHeader = document.querySelector('.logo-heading')
+
+function makeBolder(event){
+    event.target.style.fontWeight = "bolder"
+}
+
+mainHeader.addEventListener('click', makeBolder)
+
+//scroll
+
+document.html.addEventListener('scroll', function (event){
+    event.target.style.backgroundColor = "pink"
+})
